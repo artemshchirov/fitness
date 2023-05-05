bf85917acc9bd8d215c4cfed458fc19a0fae4c65
Artem Shchirov <artemshchirov@gmail.com>
2023-05-05 14:45:22 +0300

    feat(all): implement InfoMessage, Training, Running, SportsWalking, Swimming classes

---
+++ main.py
@@ -0,0 +1,186 @@
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class InfoMessage:
+    """Information message about training."""
+
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    INFO: str = ('Training type: {training_type}; '
+                 'Duration: {duration:.3f} hr.; '
+                 'Distance: {distance:.3f} km; '
+                 'Avg. speed: {speed:.3f} km/hr; '
+                 'Calories burned: {calories:.3f}.')
+
+    def get_message(self) -> str:
+        return self.INFO.format(**asdict(self))
+
+
+class Training:
+    """Base class for training."""
+
+    LEN_STEP: float = 0.65
+    M_IN_KM: float = 1000
+    M_TO_HOUR: float = 60
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+
+    def get_distance(self) -> float:
+        """Get distance in kilometers."""
+        return self.action * self.LEN_STEP / self.M_IN_KM
+
+    def get_mean_speed(self) -> float:
+        """Get average movement speed."""
+        distance = self.get_distance()
+        return distance / self.duration
+
+    def get_burned_calories(self) -> float:
+        """Get the amount of calories spent."""
+        raise NotImplementedError('unexpectedly not implemented')
+
+    def show_training_info(self) -> InfoMessage:
+        """Return an information message about the completed training."""
+        distance = self.get_distance()
+        spent_calories = self.get_burned_calories()
+        speed_km_h = self.get_mean_speed()
+        training_type = self.__class__.__name__
+
+        return InfoMessage(training_type,
+                           self.duration,
+                           distance,
+                           speed_km_h,
+                           spent_calories)
+
+
+class Running(Training):
+    """Training: Running."""
+
+    CALORIES_MEAN_SPEED_MULTIPLIER: float = 18
+    CALORIES_MEAN_SPEED_SHIFT: float = 20
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float) -> None:
+        super().__init__(action, duration, weight)
+
+    def get_burned_calories(self) -> float:
+        mean_speed_kmh = self.get_mean_speed()
+        calories_from_speed = (self.CALORIES_MEAN_SPEED_MULTIPLIER
+                               * mean_speed_kmh
+                               - self.CALORIES_MEAN_SPEED_SHIFT)
+        calories_from_distance = (self.weight / Training.M_IN_KM
+                                  * self.duration * Training.M_TO_HOUR)
+        burned_calories = calories_from_speed * calories_from_distance
+
+        return burned_calories
+
+
+class SportsWalking(Training):
+    """Training: Sports walking."""
+
+    CALORIES_MEAN_SPEED_MULTIPLIER: float = 0.035
+    CALORIES_MEAN_SPEED_SHIFT: float = 0.029
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 height: float) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
+
+    def get_mean_speed(self) -> float:
+        distance = self.get_distance()
+
+        return distance / (self.duration * 60 * 60)
+
+    def get_burned_calories(self) -> float:
+        mean_speed = self.get_mean_speed()
+        burned_calories = ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.weight
+                            + (mean_speed ** 2 // self.height)
+                            * self.CALORIES_MEAN_SPEED_SHIFT * self.weight)
+                           * self.duration * self.M_TO_HOUR)
+
+        return burned_calories
+
+
+class Swimming(Training):
+    """Training: Swimming."""
+
+    LEN_STEP = 1.38
+    CALORIES_MEAN_SPEED_MULTIPLIER = 1.1
+    CALORIES_MEAN_SPEED_SHIFT = 2
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 length_pool: float,
+                 count_pool: float) -> None:
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
+    def get_mean_speed(self):
+        distance = self.length_pool * self.count_pool / Training.M_IN_KM
+        return distance / self.duration
+
+    def get_burned_calories(self):
+        speed_kmh = self.get_mean_speed()
+        burned_calories = ((speed_kmh + self.CALORIES_MEAN_SPEED_MULTIPLIER)
+                           * self.CALORIES_MEAN_SPEED_SHIFT * self.weight)
+
+        return burned_calories
+
+
+def read_package(workout_type: str, data: list) -> Training:
+    """Read data received from sensors."""
+    training_types = {'SWM': Swimming,
+                      'WLK': SportsWalking,
+                      'RUN': Running}
+
+    try:
+        training_type = training_types[workout_type]
+    except KeyError:
+        print('Invalid workout type:', workout_type)
+        return None
+
+    try:
+        return training_type(*data)
+    except TypeError:
+        print('Incorrect number of data items')
+        return None
+
+
+def main(training: Training) -> None:
+    """Main function."""
+    try:
+        info = training.show_training_info()
+    except AttributeError:
+        print('Attribute Error')
+    else:
+        print(info.get_message())
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,24 +6,24 @@ import pytest
 from conftest import Capturing
 
 try:
-    import homework
+    import main
 except ModuleNotFoundError:
-    assert False, 'File with homework `homework.py` not found'
+    assert False, 'File `main.py` not found'
 except NameError as exc:
     name = re.findall("name '(\w+)' is not defined", str(exc))[0]
-    assert False, f'Class {name} not found in the homework file.'
+    assert False, f'Class {name} not found in the main.py file.'
 except ImportError:
-    assert False, 'File with homework `homework.py` not found'
+    assert False, 'File `main.py` not found'
 
 
 def test_read_package():
-    assert hasattr(homework, 'read_package'), (
+    assert hasattr(main, 'read_package'), (
         'Create a function to handle incoming package - `read_package`'
     )
-    assert callable(homework.read_package), (
+    assert callable(main.read_package), (
         'Make sure `read_package` is a function.'
     )
-    assert isinstance(homework.read_package, types.FunctionType), (
+    assert isinstance(main.read_package, types.FunctionType), (
         'Make sure `read_package` is a function.'
     )
 
@@ -34,7 +34,7 @@ def test_read_package():
     (('WLK', [9000, 1, 75, 180]), 'SportsWalking'),
 ])
 def test_read_package_return(input_data, expected):
-    result = homework.read_package(*input_data)
+    result = main.read_package(*input_data)
     assert result.__class__.__name__ == expected, (
         'Function `read_package` should return a sports type class '
         'depending on the training code.'
@@ -42,10 +42,10 @@ def test_read_package_return(input_data, expected):
 
 
 def test_InfoMessage():
-    assert inspect.isclass(homework.InfoMessage), (
+    assert inspect.isclass(main.InfoMessage), (
         'Make sure `InfoMessage` is a class.'
     )
-    info_message = homework.InfoMessage
+    info_message = main.InfoMessage
     info_message_signature = inspect.signature(info_message)
     info_message_signature_list = list(info_message_signature.parameters)
     for p in ['training_type', 'duration', 'distance', 'speed', 'calories']:
@@ -58,28 +58,28 @@ def test_InfoMessage():
 @pytest.mark.parametrize('input_data, expected', [
     (['Swimming', 1, 75, 1, 80],
         'Training type: Swimming; '
-        'Duration: 1.000 h.; '
+        'Duration: 1.000 hr.; '
         'Distance: 75.000 km; '
-        'Avg. speed: 1.000 km/h; '
-        'Calories spent: 80.000.'
+        'Avg. speed: 1.000 km/hr; '
+        'Calories burned: 80.000.'
      ),
     (['Running', 4, 20, 4, 20],
         'Training type: Running; '
-        'Duration: 4.000 h.; '
+        'Duration: 4.000 hr.; '
         'Distance: 20.000 km; '
-        'Avg. speed: 4.000 km/h; '
-        'Calories spent: 20.000.'
+        'Avg. speed: 4.000 km/hr; '
+        'Calories burned: 20.000.'
      ),
     (['SportsWalking', 12, 6, 12, 6],
         'Training type: SportsWalking; '
-        'Duration: 12.000 h.; '
+        'Duration: 12.000 hr.; '
         'Distance: 6.000 km; '
-        'Avg. speed: 12.000 km/h; '
-        'Calories spent: 6.000.'
+        'Avg. speed: 12.000 km/hr; '
+        'Calories burned: 6.000.'
      ),
 ])
 def test_InfoMessage_get_message(input_data, expected):
-    info_message = homework.InfoMessage(*input_data)
+    info_message = main.InfoMessage(*input_data)
     assert hasattr(info_message, 'get_message'), (
         'Create the `get_message` method in the `InfoMessage` class.'
     )
@@ -98,16 +98,16 @@ def test_InfoMessage_get_message(input_data, expected):
         'Training type: Swimming; '
         'Duration: 1.000 hr.; '
         'Distance: 75.000 km; '
-        'Avg. speed: 1.000 km/hr; '
+        'Avg. speed: 1.000 km/hrr; '
         'Calories burned: 80.000.'
     )
 
 
 def test_Training():
-    assert inspect.isclass(homework.Training), (
+    assert inspect.isclass(main.Training), (
         'Ensure that `Training` is a class.'
     )
-    training = homework.Training
+    training = main.Training
     training_signature = inspect.signature(training)
     training_signature_list = list(training_signature.parameters)
     for param in ['action', 'duration', 'weight']:
@@ -136,7 +136,7 @@ def test_Training():
     ([1206, 12, 6], 0.7838999999999999),
 ])
 def test_Training_get_distance(input_data, expected):
-    training = homework.Training(*input_data)
+    training = main.Training(*input_data)
     assert hasattr(training, 'get_distance'), (
         'Create the `get_distance` method in the `Training` class.'
     )
@@ -156,7 +156,7 @@ def test_Training_get_distance(input_data, expected):
     ([1206, 12, 6], 0.065325),
 ])
 def test_Training_get_mean_speed(input_data, expected):
-    training = homework.Training(*input_data)
+    training = main.Training(*input_data)
     assert hasattr(training, 'get_mean_speed'), (
         'Create the `get_mean_speed` method in the `Training` class.'
     )
@@ -176,28 +176,28 @@ def test_Training_get_mean_speed(input_data, expected):
     ([420, 4, 20]),
     ([1206, 12, 6]),
 ])
-def test_Training_get_spent_calories(input_data):
-    training = homework.Training(*input_data)
-    assert hasattr(training, 'get_spent_calories'), (
-        'Create the `get_spent_calories` method in the `Training` class.'
+def test_Training_get_burned_calories(input_data):
+    training = main.Training(*input_data)
+    assert hasattr(training, 'get_burned_calories'), (
+        'Create the `get_burned_calories` method in the `Training` class.'
     )
-    assert callable(training.get_spent_calories), (
-        'Make sure `get_spent_calories` is a function.'
+    assert callable(training.get_burned_calories), (
+        'Make sure `get_burned_calories` is a function.'
     )
 
 
 def test_Training_show_training_info(monkeypatch):
-    training = homework.Training(*[720, 1, 80])
+    training = main.Training(*[720, 1, 80])
     assert hasattr(training, 'show_training_info'), (
         'Create the `show_training_info` method in the `Training` class.'
     )
 
-    def mock_get_spent_calories():
+    def mock_get_burned_calories():
         return 100
     monkeypatch.setattr(
         training,
-        'get_spent_calories',
-        mock_get_spent_calories
+        'get_burned_calories',
+        mock_get_burned_calories
     )
     result = training.show_training_info()
     assert result.__class__.__name__ == 'InfoMessage', (
@@ -207,14 +207,14 @@ def test_Training_show_training_info(monkeypatch):
 
 
 def test_Swimming():
-    assert hasattr(homework, 'Swimming'), 'Create the `Swimming` class'
-    assert inspect.isclass(homework.Swimming), (
+    assert hasattr(main, 'Swimming'), 'Create the `Swimming` class'
+    assert inspect.isclass(main.Swimming), (
         'Make sure `Swimming` is a class.'
     )
-    assert issubclass(homework.Swimming, homework.Training), (
+    assert issubclass(main.Swimming, main.Training), (
         'The `Swimming` class should inherit from the `Training` class.'
     )
-    swimming = homework.Swimming
+    swimming = main.Swimming
     swimming_signature = inspect.signature(swimming)
     swimming_signature_list = list(swimming_signature.parameters)
     for param in ['action', 'duration', 'weight', 'length_pool', 'count_pool']:
@@ -236,7 +236,7 @@ def test_Swimming():
     ([1206, 12, 6, 12, 6], 0.005999999999999999),
 ])
 def test_Swimming_get_mean(input_data, expected):
-    swimming = homework.Swimming(*input_data)
+    swimming = main.Swimming(*input_data)
     result = swimming.get_mean_speed()
     assert result == expected, (
         'Override the `get_mean_speed` method in the `Swimming` class. '
@@ -250,11 +250,11 @@ def test_Swimming_get_mean(input_data, expected):
     ([420, 4, 20, 42, 4], 45.68000000000001),
     ([1206, 12, 6, 12, 6], 13.272000000000002),
 ])
-def test_Swimming_get_spent_calories(input_data, expected):
-    swimming = homework.Swimming(*input_data)
-    result = swimming.get_spent_calories()
+def test_Swimming_get_burned_calories(input_data, expected):
+    swimming = main.Swimming(*input_data)
+    result = swimming.get_burned_calories()
     assert type(result) == float, (
-        'Override the `get_spent_calories` method in the `Swimming` class.'
+        'Override the `get_burned_calories` method in the `Swimming` class.'
     )
     assert result == expected, (
         'Check the formula for calculating the spent calories in '
@@ -264,14 +264,14 @@ def test_Swimming_get_spent_calories(input_data, expected):
 
 def test_SportsWalking():
     assert hasattr(
-        homework, 'SportsWalking'), 'Create the `SportsWalking` class.'
-    assert inspect.isclass(homework.SportsWalking), (
+        main, 'SportsWalking'), 'Create the `SportsWalking` class.'
+    assert inspect.isclass(main.SportsWalking), (
         'Check that `SportsWalking` is a class.'
     )
-    assert issubclass(homework.SportsWalking, homework.Training), (
+    assert issubclass(main.SportsWalking, main.Training), (
         'The `SportsWalking` class should inherit from the `Training` class.'
     )
-    sports_walking = homework.SportsWalking
+    sports_walking = main.SportsWalking
     sports_walking_signature = inspect.signature(sports_walking)
     sports_walking_signature_list = list(sports_walking_signature.parameters)
     for param in ['action', 'duration', 'weight', 'height']:
@@ -286,11 +286,11 @@ def test_SportsWalking():
     ([420, 4, 20, 42], 168.00000000000003),
     ([1206, 12, 6, 12], 151.20000000000002),
 ])
-def test_SportsWalking_get_spent_calories(input_data, expected):
-    sports_walking = homework.SportsWalking(*input_data)
-    result = sports_walking.get_spent_calories()
+def test_SportsWalking_get_burned_calories(input_data, expected):
+    sports_walking = main.SportsWalking(*input_data)
+    result = sports_walking.get_burned_calories()
     assert type(result) == float, (
-        'Override the `get_spent_calories` method in '
+        'Override the `get_burned_calories` method in '
         'the `SportsWalking` class.'
     )
     assert result == expected, (
@@ -300,11 +300,11 @@ def test_SportsWalking_get_spent_calories(input_data, expected):
 
 
 def test_Running():
-    assert hasattr(homework, 'Running'), 'Create the `Running` class.'
-    assert inspect.isclass(homework.Running), (
+    assert hasattr(main, 'Running'), 'Create the `Running` class.'
+    assert inspect.isclass(main.Running), (
         'Check that `Running` is a class.'
     )
-    assert issubclass(homework.Running, homework.Training), (
+    assert issubclass(main.Running, main.Training), (
         'The `Running` class should inherit from the `Training` class.'
     )
 
@@ -314,14 +314,14 @@ def test_Running():
     ([420, 4, 20], -90.1032),
     ([1206, 12, 6], -81.32032799999999),
 ])
-def test_Running_get_spent_calories(input_data, expected):
-    running = homework.Running(*input_data)
-    assert hasattr(running, 'get_spent_calories'), (
-        'Create the `get_spent_calories` method in the `Running` class.'
+def test_Running_get_burned_calories(input_data, expected):
+    running = main.Running(*input_data)
+    assert hasattr(running, 'get_burned_calories'), (
+        'Create the `get_burned_calories` method in the `Running` class.'
     )
-    result = running.get_spent_calories()
+    result = running.get_burned_calories()
     assert type(result) == float, (
-        'Override the `get_spent_calories` method in the `Running` class.'
+        'Override the `get_burned_calories` method in the `Running` class.'
     )
     assert result == expected, (
         'Check the formula for calculating spent calories in '
@@ -330,11 +330,11 @@ def test_Running_get_spent_calories(input_data, expected):
 
 
 def test_main():
-    assert hasattr(homework, 'main'), (
+    assert hasattr(main, 'main'), (
         'Create the main program function named `main`.'
     )
-    assert callable(homework.main), 'Check that `main` is a function.'
-    assert isinstance(homework.main, types.FunctionType), (
+    assert callable(main.main), 'Check that `main` is a function.'
+    assert isinstance(main.main, types.FunctionType), (
         'Check that `main` is a function.'
     )
 
@@ -342,30 +342,30 @@ def test_main():
 @pytest.mark.parametrize('input_data, expected', [
     (['SWM', [720, 1, 80, 25, 40]], [
         'Training type: Swimming; '
-        'Duration: 1.000 h.; '
+        'Duration: 1.000 hr.; '
         'Distance: 0.994 km; '
-        'Avg. speed: 1.000 km/h; '
-        'Calories spent: 336.000.'
+        'Avg. speed: 1.000 km/hr; '
+        'Calories burned: 336.000.'
     ]),
     (['RUN', [1206, 12, 6]], [
         'Training type: Running; '
-        'Duration: 12.000 h.; '
+        'Duration: 12.000 hr.; '
         'Distance: 0.784 km; '
-        'Avg. speed: 0.065 km/h; '
-        'Calories spent: -81.320.'
+        'Avg. speed: 0.065 km/hr; '
+        'Calories burned: -81.320.'
     ]),
     (['WLK', [9000, 1, 75, 180]], [
         'Training type: SportsWalking; '
-        'Duration: 1.000 h.; '
+        'Duration: 1.000 hr.; '
         'Distance: 5.850 km; '
-        'Avg. speed: 5.850 km/h; '
-        'Calories spent: 157.500.'
+        'Avg. speed: 5.850 km/hr; '
+        'Calories burned: 157.500.'
     ])
 ])
 def test_main_output(input_data, expected):
     with Capturing() as get_message_output:
-        training = homework.read_package(*input_data)
-        homework.main(training)
+        training = main.read_package(*input_data)
+        main.main(training)
     assert get_message_output == expected, (
         'The `main` method should print the result to the console.\n'
     )
